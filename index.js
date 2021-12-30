f1a204a611d9310e94a1e547f7b66b5dba6a55de
viraj02 <virujoshi02@gmail.com>
2021-12-31 02:27:08 +0530

    created basic nodejs app with route

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// import routes
+const authRoute = require('./routes/auth');
+
+// Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => {
+    console.log('Server up');
+})
