7e977dbe810dfbdff5d035f8d646529139568a59
viraj02 <virujoshi02@gmail.com>
2021-12-31 20:44:29 +0530

    Created Local MongoDB connection

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 // import routes
 const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+    `${process.env.DB_PATH}/${process.env.DB_NAME}`,
+    { useUnifiedTopology: true },
+    (err) => {
+        console.log('connected to db');
+    }
+);
+
+// Middleware
+app.use(express.json());
+
 
 // Route Middlewares
 app.use('/api/user', authRoute);
