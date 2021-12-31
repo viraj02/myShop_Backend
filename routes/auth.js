a63e648730e8c0d9c8eeb5bfd6e7b25c3fc6532a
viraj02 <virujoshi02@gmail.com>
2021-12-31 20:48:49 +0530

    Created basic route using User Schema

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+router.post('/register', async (req, res) => {
+    const reqBody = req.body;
+    const user = new User({
+        name: reqBody.name,
+        email: reqBody.email,
+        password: reqBody.password
+    });
+    
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    
 });
 
 
