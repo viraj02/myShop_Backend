9048b4fc325e66bbf30de88991f45236fca88026
viraj02 <virujoshi02@gmail.com>
2022-01-02 16:53:44 +0530

    Login code implemented using bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const { registrationValidation } = require('../validations');
+const { registrationValidation, loginValidation } = require('../validations');
 const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
@@ -31,6 +31,23 @@ router.post('/register', async (req, res) => {
         res.status(400).send(error);
     }
 
+});
+
+router.post('/login', async (req, res) => {
+    const reqBody = req.body;
+
+    const { error } = loginValidation(reqBody);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Checking if email doesn't exists
+    const user = await User.findOne({ email: reqBody.email });
+    if (!user) return res.status(400).send('Email doesn\'t exists');
+
+    // Check password
+    const validPass = await bcrypt.compare(reqBody.password, user.password);
+    if (!validPass) return res.status(400).send('Invalid Password');
+
+    res.send('Login Successful');
 });
 
 
