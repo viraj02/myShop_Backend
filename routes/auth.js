d272b60e6c6eeed23b8c279fad30b84838518699
viraj02 <virujoshi02@gmail.com>
2022-01-02 16:53:00 +0530

    Formated code spaces

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.post('/register', async (req, res) => {
 
     try {
         const savedUser = await user.save();
-        res.send({user: user._id});
+        res.send({ user: user._id });
     } catch (error) {
         res.status(400).send(error);
     }
